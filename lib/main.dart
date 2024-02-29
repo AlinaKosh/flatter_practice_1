6d8f0be562e9b5e6723701239b00803f0bc1f9c4
AlinaKosh <99735329+AlinaKosh@users.noreply.github.com>
2024-02-29 11:28:00 +0400

    result

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+       theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Мое первое приложение'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+    const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int _counter = 0;
+
+  void _incrementCounter() {
+      setState(() {
+        _counter ++;
+      });
+    }
+  void _incrementCounter2() {
+    setState(() {
+      _counter += 2;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Center(child: Text(widget.title)), // Заголовок по центру
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: _incrementCounter2,
+          ),
+        ],
+      ),
+      body: Column(
+          children: <Widget>[
+            const Padding(
+            padding: EdgeInsets.only(left: 16.0, top: 20.0), 
+            child: Text(
+              'Мы нажали: ',
+            ),
+          ),
+            Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add), //Иконка "+"
+      ),
+    );
+  }
+}
